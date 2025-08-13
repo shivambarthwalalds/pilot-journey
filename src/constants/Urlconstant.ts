b824ff560e56e13b1a078a451e09188d861fc1df
Shivam barthwal <shivam.paulbarthwal@gmail.com>
2025-08-13 17:52:28 +0530

    NewChanges

--- src/constants/Urlconstant.ts
+++
@@ -1,8 +0,0 @@
-export const base = {
-  API_URL: "https://crm.pilotspathway.in/",
-};
-
-export const urlConstants = {
-  Allblogs: `${base.API_URL}api/blog/posts`,
-  Singleblog: `${base.API_URL}api/blog/post`,
-};
